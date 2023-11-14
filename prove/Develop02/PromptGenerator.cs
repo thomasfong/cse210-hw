5b9afd5b6081a275e3767c85e49225f066e178ae
thomas fong <thomasfong1020@gmail.com>
2023-11-14 22:55:14 +0800

    Update cs

---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,12 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class GetRandomPrompt
+{
+    public List<string> _prompts;
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+}
